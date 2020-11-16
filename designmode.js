d60f3fc65592a0d39c26f8540a3c0fff7cc07ed9
Rohan Kapur <rohankapurdev@outlook.com>
2020-11-16 17:39:02 +0530

    plugin

---
+++ designmode.js
@@ -0,0 +1,7 @@
+if (document.designMode == "on") {
+  alert("Editing mode is now disabled! 😢");
+  document.designMode = "off";
+} else {
+  alert("Editing mode is now enabled! 🎉🎉🎉");
+  document.designMode = "on";
+}
